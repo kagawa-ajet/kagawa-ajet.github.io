c5c4487cb5fccd38c2c7c8eadec9567eb3b00e98
Chris Milson <chris.w.milson@gmail.com>
2020-01-30 14:07:57 +0900

    Use React
    The intention for this page is that is it written in react and built
    with create-react-app. At the moment this is just a rewrite of the
    original placeholder page.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kagawa AJET
+
+
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). 
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.scss'
+
+export default function App () {
+  return (
+    <div className='App'>
+      <h1>Welcome to Kagawa AJET</h1>
+      <p>
+        This is a static page. The first in a long line!
+      </p>
+      <p>
+        We decided that we didnt need the extra information; you can get it a
+        LOT of other places. This page should just be about events going on and
+        around Kagawa.
+      </p>
+    </div>
+  )
+}
