6640caa687ae94b6af1abe8967543a40c66626cb
Chris Milson <chris.w.milson@gmail.com>
2020-02-04 13:43:33 +0900

    Use calendar hook
    By using the calendar hook instead of the events hook, we get more
    control over the api, and can do things like only revealing the next
    event, instead of all of the events.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import { useEvents } from './calendar-api'
+import React, { useEffect, useState } from 'react'
+import { useCalendar } from './calendar-api'
 import Event from './components/Event'
 import './App.scss'
 
 export default function App () {
-  const events = useEvents(
+  const calendar = useCalendar(
     'gprr6e1so5bm32gjig3vf5ehk8@group.calendar.google.com',
     'AIzaSyAT6OXBzAOkHsAjt61M-MmQESV_DJOXeLs'
   )
 
+  const [nextEvent, setNext] = useState()
+
+  useEffect(() => {
+    if (!calendar) return
+
+    /**
+     * Will be changed to false when this effect goes out of scope (the calendar
+     * changes), and will cause the promise to do nothing.
+     */
+    let valid = true
+    calendar.getNext()
+      .then(res => {
+        if (!valid) return
+
+        // get the event
+        if (res.items.length > 0) setNext(res.items[0])
+        else {
+          // or there are no future events.
+          setNext({
+            summary: 'Oh dear....',
+            description: 'There are no future events.'
+          })
+        }
+      })
+    return () => { valid = false }
+  }, [calendar])
+
   return (
     <div className='App'>
-      {events && events.map(event => <Event key={event.id} {...event} />)}
+      {nextEvent && <Event {...nextEvent} />}
     </div>
   )
 }
